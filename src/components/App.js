cbf750b351d441eb4ab583594079f48db4e681b8
Chirag Swadia <chirag.swadia@auto1.com>
2019-12-16 15:19:34 +0100

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
+import CreateLink from './CreateLink'
+import LinkList from './LinkList'
+import Header from './Header'
+import Login from './Login'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Switch>
+          <Route exact path="/" component={LinkList} />
+          <Route exact path="/create" component={CreateLink} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </div>
+    </div>
+    )
+  }
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom'
+import './styles/index.css';
+import App from './components/App';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from 'apollo-link-context'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import { AUTH_TOKEN } from './constants'
+
+const httpLink = createHttpLink({
+    uri: 'http://localhost:4000'
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
+const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
+})
+
+ReactDOM.render(
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </BrowserRouter>,
+    document.getElementById('root')
+  )
+  serviceWorker.unregister();
